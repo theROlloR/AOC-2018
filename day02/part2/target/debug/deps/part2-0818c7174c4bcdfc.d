b3d6cad25167b1742586e2be9c19aaf9af1605f3
theROlloR <45531941+theROlloR@users.noreply.github.com>
2018-12-02 12:15:09 +0100

    Release

---
+++ day02/part2/target/debug/deps/part2-0818c7174c4bcdfc.d
@@ -0,0 +1,6 @@
+C:\Users\Raul\Desktop\Advent of Code\AOC-2018\day02\part2\target\debug\deps\part2-0818c7174c4bcdfc.exe: src\main.rs src\input.txt
+
+C:\Users\Raul\Desktop\Advent of Code\AOC-2018\day02\part2\target\debug\deps\part2-0818c7174c4bcdfc.d: src\main.rs src\input.txt
+
+src\main.rs:
+src\input.txt:
